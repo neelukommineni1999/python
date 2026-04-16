064510910ec9457688d592cf207e942a50f83cf4
neelukommineni1999 <neelukommineni1999@gmail.com>
2026-04-16 09:05:12 -0400

    1

--- Exercise6.py
+++ Exercise6.py
@@ -1,5 +1,14 @@
+# Exercise 6: Format Product Name
 def format_product(name):
     return name.upper()
 
 product = format_product("laptop")
 print(product)
+
+# using functions early instead of writing everything in one big block of code.
+def format_product(name):
+    return name.upper()
+
+print(format_product("laptop"))
+print(format_product("phone"))
+print(format_product("tablet"))
